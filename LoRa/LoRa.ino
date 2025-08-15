6378959abe50ee2e3ec4ff9ebd4ba60053b4102b
principia137 <yuyu7020@cau.ac.kr>
2025-08-15 22:42:23 +0900

    LoRa communication using UART

--- LoRa/LoRa.ino
+++ LoRa/LoRa.ino
@@ -1,31 +1,32 @@
 // #include <LoRa.h>
 // #include <SPI.h>
-#include <SoftwareSerial.h> //Rx, Tx에 연결된 모듈과 UART 통신
+#include <SoftwareSerial.h> // Rx, Tx에 연결된 모듈과 UART 통신
 
-//SoftwareSerial(Rx, Tx)
-SoftwareSerial lora(2, 3);
+SoftwareSerial lora(2, 3);// SoftwareSerial(Rx, Tx)
+const char EN = 4;  // LoRa 모듈 활성화(Enable) 핀
+const char AUX = 5; // 보조(Auxiliary) 핀 (코드에서는 사용되지 않음)
 
 void setup() {
   Serial.begin(9600);
   lora.begin(9600);
+  pinMode(EN, OUTPUT);
+  digitalWrite(EN, LOW); // LOW:작동, HIGH:절전모드
 }
 
 void loop() {
   //송신 코드
-  while (Serial.available() < 1);
-  while (Serial.available() > 0){
-    char data;
-    data = Serial.read();
-    lora.write(data) //데이터를 가공하지 않고 1바이트씩 보냄
-    // lora.println("Hello, Lora!\n") //문자열 형태로 데이터를 보냄
+  if (Serial.available()){
+    String message_send;
+    message_send = Serial.readStringUntil('\n');
+    //lora.write(message_send) // 데이터를 가공하지 않고 1바이트씩 보냄
+     lora.println(message_send); // 문자열 형태로 데이터를 보냄
+     Serial.println("sent:"+message_send);
   }
   
   //수신 코드
-  while (lora.available() < 1);
-  while (lora.available() > 0){
-    char data;
-    data = lora.read();
-    Serial.write(data); 
+  if (lora.available()){
+    String message_recieved;
+    message_recieved = lora.readStringUntil('\n');
+    Serial.println(message_recieved); 
   }
-
 }
